475f37763813701cfec7520f146361889b1f89fc
andreusola <140717026+andreusola@users.noreply.github.com>
2025-07-29 19:27:44 +0200

    Day 37: Send and receive an email with the image

--- emailing.py
+++ emailing.py
@@ -1,2 +1,26 @@
-def send_email():
-    print("Email was sent!")
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
+password = "kxsy mejq thbb bfsv"
+SENDER = "andreusdinversions@gmail.com"
+RECEIVER = SENDER
+
+def send_email(image_path):
+    email_message = EmailMessage()
+    email_message["Subject"] = "New costumer showed up!"
+    email_message.set_content("Hey, we just saw a new costumer!")
+
+    with open(image_path, 'rb') as file:
+        content = file.read()
+    email_message.add_attachment(content, maintype="image",
+                                 subtype=imghdr.what(None, content))
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, password)
+    gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
+    gmail.quit()
+
+if __name__ == "__main__":
+    send_email(image_path="images/19.png")
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2 #cv2 collaborates with numpy library
 import time
 from emailing import send_email
+import glob
 
 video = cv2.VideoCapture(0)  # 0 because I only have 1 (main) camera in my laptop
 time.sleep(1)  # code waits 1 sec
@@ -8,6 +9,7 @@ time.sleep(1)  # code waits 1 sec
 
 first_frame = None
 status_list = []
+count = 1
 while True:
     status = 0
     check, frame = video.read()
@@ -43,12 +45,18 @@ while True:
         rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images) / 2)
+            image_with_object = all_images[index]
+
 
     status_list.append(status)
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        send_email(image_with_object)
 
         cv2.imshow("Video", frame)
 
